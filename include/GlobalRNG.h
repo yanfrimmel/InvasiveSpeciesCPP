2f88a88b0eb919b82a2f3f045b6663f8c159800b
yanfrimmel <yanfrimmel1411@gmail.com>
2020-05-01 14:40:29 +0300

    Fixed MSC compitability

--- include/GlobalRNG.h
+++ include/GlobalRNG.h
@@ -5,9 +5,9 @@
 #include <random>
 
 namespace globalRNG {
-#ifdef __unix__
-    extern std::_Bind<std::uniform_real_distribution<float>(std::mt19937 &)> rng;
+#ifdef _MSC_VER
+	extern std::_Binder<std::_Unforced, std::uniform_real_distribution<float>, std::mt19937 &> rng;
 #elif defined(_WIN32) || defined(WIN32)
-    extern std::_Binder<std::_Unforced, std::uniform_real_distribution<float>, std::mt19937 &> rng;
+	extern std::_Bind<std::uniform_real_distribution<float>(std::mt19937 &)> rng;
 #endif
 }
